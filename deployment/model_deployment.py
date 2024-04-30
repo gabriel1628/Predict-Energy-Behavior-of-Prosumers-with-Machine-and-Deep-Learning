6b59015291452062b9f9d832e28f02795f223678
gabriel1628 <gabriel_hc@hotmail.fr>
2024-04-30 15:55:34 +0200

    Rename columns
    Rename `unique_id`, `ds`, `y`, `values` and `predictions` columns into `building_id`, `datetime`, `consumption`, `consumption_values` and `forecasts` respectively.
    Also rename the field `prediction` in json output into `predictions` and merge output values into a single field called `forecasts`

--- deployment/model_deployment.py
+++ deployment/model_deployment.py
@@ -26,19 +26,23 @@ def predict():
 
     # Put the data in a dataframe and create features
     input_data = pd.DataFrame(data)
-    input_data[["y", "lag1", "lag2"]] = pd.DataFrame(
-        input_data["values"].tolist(), index=input_data.index
+    input_data[["consumption", "lag1", "lag2"]] = pd.DataFrame(
+        input_data["consumption_values"].tolist(), index=input_data.index
     )
-    input_data = input_data.drop(columns="values")
+    input_data = input_data.drop(columns="consumption_values")
 
     # Make prediction using model loaded from disk as per the data.
     preds_df = pd.DataFrame(
         pipeline.predict(input_data), index=input_data.index, columns=["lead1", "lead2"]
     )
     predictions = pd.concat([input_data.iloc[:, :2], preds_df], axis=1)
+    predictions["forecasts"] = pd.Series(
+        predictions[["lead1", "lead2"]].values.tolist()
+    )
+    predictions = predictions.drop(columns=["lead1", "lead2"])
     # Reformat predictions in json
     predictions_json = predictions.to_json(orient="records", indent=4)
-    output = json.dumps({"prediction": json.loads(predictions_json)}, indent=4)
+    output = json.dumps({"predictions": json.loads(predictions_json)}, indent=4)
 
     return jsonify(output)
 
